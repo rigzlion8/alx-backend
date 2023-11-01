55e3b89a993631341992bc74e04a702e6b93b7ae
Reagan Musa <reagan.musav@gmail.com>
2023-11-01 11:45:00 +0000

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,37 +1,36 @@
-#!/usr/bin/env python3
-
-''' 1- FIFO caching
-'''
-
-
-from collections import OrderedDict
+#!/usr/bin/python3
+"""
+1 - Module 1-fifo_cache.py
+"""
 from base_caching import BaseCaching
+from collections import deque
 
 
 class FIFOCache(BaseCaching):
-    '''A class `FIFOCache` that inherits from
-       `BaseCaching` and is a caching system.
-    '''
+    """
+    Overrides put() nad self()
+    from parent
+    """
 
     def __init__(self):
+        """initialize"""
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.queue = deque([])
 
     def put(self, key, item):
-        '''assign to the dictionary `self.cache_data` the
-           `item` value for the key `key`
-        '''
-
-        if key is None or item is None:
-            return
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
-
-        self.cache_data[key] = item
+        """Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS\
+                    and key not in self.queue:
+                del_key = self.queue.popleft()
+                del self.cache_data[del_key]
+                print("DISCARD: {}".format(del_key))
+            self.cache_data[key] = item
+            self.queue.append(key)
 
     def get(self, key):
-        '''return the value in `self.cache_data` linked to `key`
-        '''
-        return self.cache_data.get(key, None)
+        """Get an item by key
+        """
+        value = self.cache_data.get(key)
+        return value
